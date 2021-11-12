28fe46c8fb912eccbfd127424ad3d0e0c4270bfb
hoge <hoge821@users.noreply.github.com>
2021-11-13 00:12:41 +0900

    フレームの大きさ変更

--- src/shooting/Shooting.java
+++ src/shooting/Shooting.java
@@ -1,7 +1,7 @@
 package shooting;
 
 public class Shooting {
-    
+
     public static void main(String args[]) {
         ShootingFrame sf = new ShootingFrame();
     }
--- src/shooting/ShootingFrame.java
+++ src/shooting/ShootingFrame.java
@@ -6,7 +6,7 @@ public class ShootingFrame extends JFrame {
     public ShootingFrame() {
         this.setTitle("Shooting");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(800,800);
+        this.setSize(500,500);
         this.setLocationByPlatform(true);//フレームの表示位置をOSに任せる
         this.setVisible(true);
     }
