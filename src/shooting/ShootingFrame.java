22bedf9400dfb42e430968197e2210695dc70b1f
hoge <hoge821@users.noreply.github.com>
2021-11-12 23:59:23 +0900

    フレーム作成処理の追加

--- src/shooting/ShootingFrame.java
+++ src/shooting/ShootingFrame.java
@@ -3,5 +3,11 @@ package shooting;
 import javax.swing.*;
 
 public class ShootingFrame extends JFrame {
-    
+    public ShootingFrame() {
+        this.setTitle("Shooting");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800,800);
+        this.setLocationByPlatform(true);//フレームの表示位置をOSに任せる
+        this.setVisible(true);
+    }
 }
